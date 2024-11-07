9805e4d223706d91e6337dd49b30b1ca9e49fae0
Christopher Chang <cchang6066@stu.d214.org>
2024-11-07 07:29:57 -0800

    i

--- JimmyVLCT/JimmyVLCT/ContentView.swift
+++ JimmyVLCT/JimmyVLCT/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Test 1")
+            Text("Test 2")
         }
         .padding()
     }
