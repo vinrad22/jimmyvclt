78033367144d83b74b32ff7da45723fa0b1c829b
vinrad22 <vradlicz6377@stu.d214.org>
2024-11-07 09:11:08 -0600

    Add files via upload
    Add files

---
+++ JimmyVLCT/JimmyVLCT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JimmyVLCT
+//
+//  Created by Vincent M. Radlicz on 11/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JimmyVLCT/JimmyVLCT/JimmyVLCTApp.swift
@@ -0,0 +1,17 @@
+//
+//  JimmyVLCTApp.swift
+//  JimmyVLCT
+//
+//  Created by Vincent M. Radlicz on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct JimmyVLCTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
